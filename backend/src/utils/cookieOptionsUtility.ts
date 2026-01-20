c0e9b62eeff9c80f96e20c88eee7ee103b9e20c3
vishnu <getvishnuvs@gmail.com>
2026-01-20 17:56:47 +0530

    fix:cookie options

--- backend/src/utils/cookieOptionsUtility.ts
+++ backend/src/utils/cookieOptionsUtility.ts
@@ -3,9 +3,9 @@ import { CookieOptions } from 'express';
 export class CookieOptionsUtility {
   static create(maxAge?: number): CookieOptions {
     return {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+     httpOnly: true,
+      secure: true, 
+      sameSite: 'none', 
       maxAge,
     };
   }
